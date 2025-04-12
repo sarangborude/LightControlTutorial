bd6bb8d2ae0a302fd0a744ed8640d1d0b249a6ff
sarangborude <sarang.borude@gmail.com>
2025-04-12 08:08:10 -0700

    Initial Commit

---
+++ LightControlTutorial/LightControlTutorialApp.swift
@@ -0,0 +1,33 @@
+//
+//  LightControlTutorialApp.swift
+//  LightControlTutorial
+//
+//  Created by Sarang Borude on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LightControlTutorialApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
